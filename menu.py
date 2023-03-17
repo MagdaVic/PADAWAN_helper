3886494b75046cd3a7a94b2e51d6f4c86a8053c2
Victoria Magda <119420321+MagdaVic@users.noreply.github.com>
2023-03-17 19:18:36 +0300

    complete_while_typing=True

--- menu.py
+++ menu.py
@@ -15,7 +15,7 @@ print('Hello! I am your personal PADAWAN helper')
 print("How can I help you?")
 while True:
     commands_string = prompt(
-            'Enter what do you want to do:',completer=command_completer,complete_while_typing=False).lstrip()
+            'Enter what do you want to do:',completer=command_completer,complete_while_typing=True).lstrip()
     for i in COMMANDS.keys():
         if commands_string.lower().startswith(i):
             command = commands_string[:len(i)].lower()
