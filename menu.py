fd3beccb0c5dc4bd5300d38ecea1541044e6b6e0
Victoria Magda <119420321+MagdaVic@users.noreply.github.com>
2023-03-17 11:42:18 +0300

    Add command addressbook into menu

--- menu.py
+++ menu.py
@@ -2,11 +2,12 @@ import sys
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 from sort import main as sort
+from addressbook import main as addressbook
 
 def exit_from_chat():
     sys.exit('Good bye!')
 
-COMMANDS={'sort directory':sort,'exit': exit_from_chat}
+COMMANDS={'use addressbook':addressbook,'sort directory':sort,'exit': exit_from_chat}
 
 command_completer = WordCompleter(COMMANDS.keys(),ignore_case=True)
 print('Hello! I am your personal PADAWAN helper')
