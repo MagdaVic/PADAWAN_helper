c3a04da0f186dd1b9457188198a1ef71155917c2
Victoria Magda <119420321+MagdaVic@users.noreply.github.com>
2023-03-17 17:27:46 +0300

    Import notebook to menu

--- menu.py
+++ menu.py
@@ -3,11 +3,12 @@ from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 from sort import main as sort
 from addressbook import main as addressbook
+from notebook import main as notebook 
 
 def exit_from_chat():
     sys.exit('Good bye!')
 
-COMMANDS={'use addressbook':addressbook,'sort directory':sort,'exit': exit_from_chat}
+COMMANDS={'use addressbook':addressbook,'use notebook':notebook,'sort directory':sort,'exit': exit_from_chat}
 
 command_completer = WordCompleter(COMMANDS.keys(),ignore_case=True)
 print('Hello! I am your personal PADAWAN helper')
