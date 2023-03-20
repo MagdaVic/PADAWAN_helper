f3c01d91494e8c11d93fe7d91fbddf88edd79b50
Victoria Magda <119420321+MagdaVic@users.noreply.github.com>
2023-03-20 20:01:13 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -13,19 +13,23 @@ def exit_from_chat():
     sys.exit('    > Good bye!')
 
 
-COMMANDS = {'use addressbook': addressbook, 'use notebook': notebook,
-            'sort directory': sort_directory, 'exit': exit_from_chat}
+def main():
+    COMMANDS = {'use addressbook': addressbook, 'use notebook': notebook,
+                'sort directory': sort_directory, 'exit': exit_from_chat}
 
-command_completer = WordCompleter(COMMANDS.keys(), ignore_case=True)
-print(Fore.YELLOW + Back.BLUE + ' Glory to  Ukraine! ')
-print(Fore.BLUE + Back.YELLOW + 'Glory to the Heroes!')
-print('    > Hello! I am your personal PADAWAN helper')
-print("    > How can I help you?")
-while True:
-    commands_string = prompt(
-        '    > Enter what do you want to do:', completer=command_completer, complete_while_typing=True).lstrip()
-    for i in COMMANDS.keys():
-        if commands_string.lower().startswith(i):
-            command = commands_string[:len(i)].lower()
-            COMMANDS[command]()
-            break
+    command_completer = WordCompleter(COMMANDS.keys(), ignore_case=True)
+    print(Fore.YELLOW + Back.BLUE + ' Glory to  Ukraine! ')
+    print(Fore.BLUE + Back.YELLOW + 'Glory to the Heroes!')
+    print('    > Hello! I am your personal PADAWAN helper')
+    print("    > How can I help you?")
+    while True:
+        commands_string = prompt(
+            '    > Enter what do you want to do:', completer=command_completer, complete_while_typing=True).lstrip()
+        for i in COMMANDS.keys():
+            if commands_string.lower().startswith(i):
+                command = commands_string[:len(i)].lower()
+                COMMANDS[command]()
+                break
+
+if __name__ == '__main__':
+    main()
